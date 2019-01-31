1c020f46dc70abf5248decfaba907c7155d1bfa4
ZK Liu <zkliu@ZKs-Mac.local>
2019-01-31 10:50:39 -0800

    complete app2 set-up

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    }
+  }
+
+
+  render () {
+    return (
+    <div>
+      <h1>Mini Apps 2 Challenge 2</h1>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
