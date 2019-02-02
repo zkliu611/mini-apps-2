b836d11ad0e20b02b307eb4f70bb8ee8d2a81f44
ZK Liu <zkliu@ZKs-Mac.local>
2019-02-02 08:24:19 -0800

    final push app3

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ScorePad from './components/ScorePad.jsx';
 import ScoreBoard from './components/ScoreBoard.jsx';
-import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
